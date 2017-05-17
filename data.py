094481c5a9970cdb6ed3be921763c060df6eb6ac
Tyler Trine <trine.tyler@gmail.com>
2017-05-17 13:32:54 -0400

    Train/val split is now contiguous subarrays

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-import csv, math
+import csv, math, random
 import numpy as np
 
-from sklearn.model_selection import train_test_split
+random.seed(42)
 
 def load(dataset):
 	X = np.load('data/'+dataset+'.npy')
@@ -15,9 +15,22 @@ def load(dataset):
 	return X
 
 def split(X, y):
-	''' Set aside 20% of the data for training. '''
-	return train_test_split(
-		X, y, test_size=0.2, random_state=42)
+	''' Set aside 20% of the data for training. 
+		Data are partitioned into contiguous subregions 
+		to reduce validation overfitting.'''
+	while True:
+		s = [[e, e+408] for e in random.sample(range(len(X)),10)]
+		s.sort(key=lambda x: x[0])
+		if s[-1][1] >= 20400: # Check boundary condition
+			continue
+		if all([s[i][1] < s[i+1][0] for i in range(len(s)-1)]): # Check non-overlapping condition
+			break
+		else: continue
+		break
+
+	val_inds = sum([range(e[0],e[1]) for e in s],[])
+	train_inds = [e for e in range(len(X)) if e not in val_inds]
+	return X[train_inds], X[val_inds], y[train_inds], y[val_inds]
 
 def stack(X, y=None):
 	# Turn X from (samples, length, width, 3)
